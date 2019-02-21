aa215654d429af17309c9ac26c0fa2985749240e
mrfelipenoronha <felipe.castro.noronha@hotmail.com>
2019-02-20 22:34:57 -0300

    First commit

---
+++ EP01/Combinacao.java
@@ -0,0 +1,118 @@
+/****************************************************************
+    Nome: Felipe Castro de Noronha
+    NUSP: 10737032
+
+    Ao preencher esse cabeçalho com o meu nome e o meu número USP,
+    declaro que todas as partes originais desse exercício programa (EP)
+    foram desenvolvidas e implementadas por mim e que portanto não 
+    constituem desonestidade acadêmica ou plágio.
+    Declaro também que sou responsável por todas as cópias desse
+    programa e que não distribui ou facilitei a sua distribuição.
+    Estou ciente que os casos de plágio e desonestidade acadêmica
+    serão tratados segundo os critérios divulgados na página da 
+    disciplina.
+    Entendo que EPs sem assinatura devem receber nota zero e, ainda
+    assim, poderão ser punidos por desonestidade acadêmica.
+
+    Abaixo descreva qualquer ajuda que você recebeu para fazer este
+    EP.  Inclua qualquer ajuda recebida por pessoas (inclusive
+    monitoras e colegas). Com exceção de material de MAC0323, caso
+    você tenha utilizado alguma informação, trecho de código,...
+    indique esse fato abaixo para que o seu programa não seja
+    considerado plágio ou irregular.
+
+    Descrição de ajuda ou indicação de fonte:
+
+
+
+    Se for o caso, descreva a seguir 'bugs' e limitações do seu programa:
+
+****************************************************************/
+
+/******************************************************************************
+ *  Compilation:  javac-algs4 Combinacao.java
+ *  Execution:    java Combinacao n k [opcao]
+ *  
+ *  Enumera todas as combinações dos números em {1,2,...,n} k a k.
+ *  Se opcao = 0 (defaul), gera e exibe todas as permutações em ordem 
+ *  lexicográfica
+ *  Se opcao = 1 apenas, __gera todas__ as combinações, mas __não__ as 
+ *  exibe; apenas exibe o total de combinações.
+ *
+ * % java Combinacao 5 3 1
+ * 10
+ * elapsed time = 0.002
+ * % java Combinacao 5 3 
+ * 1 2 3 
+ * 1 2 4 
+ * 1 2 5 
+ * 1 3 4 
+ * 1 3 5 
+ * 1 4 5 
+ * 2 3 4 
+ * 2 3 5 
+ * 2 4 5 
+ * 3 4 5 
+ * 10
+ * elapsed time = 0.002
+ * % java Combinacao 100 3 1
+ * 161700
+ * elapsed time = 0.004
+ * % java Combinacao 1000 3 1
+ * 166167000
+ * elapsed time = 0.726
+ *   
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class Combinacao {
+    private static int count = 0; // contador de combinações
+    private static int opcao = 0; 
+        // 1 imprimir apenas o número de combinações (default)
+        // 0 imprimir as combinações e o número de combinações
+
+    private static int numbers[]; // Vai armazenar a ultima combinação impressa
+
+    private static void gerador(int ind, int atual, int k, int n){
+
+    	if (ind == k){
+
+    		count++; // Incrementando contador
+    		if (opcao == 1) return; // Retornando caso a opcao seja 0
+
+    		// Imprimo a combinação
+    		for (int i = 0; i < k; i++)
+    			StdOut.printf("%d " ,numbers[i]);
+    		StdOut.println();
+    		
+    		return;
+    	}
+
+    	for (int a = atual; a <= n; a++){
+    		// Percorro todos os numeros possiveis no intervalo [a, n]
+    		numbers[ind] = a;
+    		gerador(ind+1, a+1, k, n); // Continuando a geração
+    	}
+    }
+
+    public static void combinacao(int n, int k) {
+
+    	numbers = new int[k]; // Inicializando esse vetor com k posições
+
+    	gerador(0, 1, k, n); // Chamando a função que gera as combinaçẽs
+    }
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+        if (args.length == 3) {
+            opcao = Integer.parseInt(args[2]);
+        }
+        Stopwatch timer = new Stopwatch();
+        combinacao(n, k);
+        StdOut.println(count);
+        StdOut.println("elapsed time = " + timer.elapsedTime());
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# MAC0323 - Algoritmos e estruturas de dados II
+
+Códigos, aula e projetos desenvolvidos na disciplina MAC0323, no Instituto de Matematica e Estatistica da Universidade de São Paulo.
