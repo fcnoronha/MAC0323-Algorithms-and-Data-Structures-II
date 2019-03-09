ab3b34381b6ed7b6dd078ad43e86bcb505e78cd5
mrfelipenoronha <felipe.castro.noronha@hotmail.com>
2019-03-09 14:04:49 -0300

    EP04 - Linked list strikes back

--- EP04/Deque.java
+++ EP04/Deque.java
@@ -1,3 +1,4 @@
+
 /****************************************************************
     Nome: Felipe Castro de Noronha
     NUSP: 10737032
@@ -51,35 +52,35 @@ public class Deque<Item> implements Iterable<Item> {
     private Node nFront; // Front (End of ll)
     private Node nBack; // Back (Start of ll)
 
-    // construct an empty deque
-    public Deque(){
+    // Construct an empty deque
+    public Deque() {
+
         nFront = nBack = null;
         size = 0;
     }
 
-    // is the deque empty?
-    public boolean isEmpty(){
+    // Is the deque empty?
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    // return the number of items on the deque
-    public int size(){
+    // Return the number of items on the deque
+    public int size() {
         return this.size;
     }
 
-    // add the item to the front
-    public void addFirst(Item item){
+    // Add the item to the front
+    public void addFirst(Item item) {
 
+        size++;
         if (nFront == null) {
             nFront = new Node();
             nFront.item = item;
             nBack = nFront;
-            size++;
 
             return;
         }
 
-        size++;
         Node aux = new Node();
         aux.item = item;
         nFront.next = aux;
@@ -88,19 +89,18 @@ public class Deque<Item> implements Iterable<Item> {
         aux.prev = aux2;
     }
 
-    // add the item to the back
-    public void addLast(Item item){
+    // Add the item to the back
+    public void addLast(Item item) {
 
+        size++;
         if (nBack == null) {
             nBack = new Node();
             nBack.item = item;
             nFront = nBack;
-            size++;
 
             return;
         }
 
-        size++;
         Node aux = new Node();
         aux.item = item;
         nBack.prev = aux;
@@ -109,8 +109,8 @@ public class Deque<Item> implements Iterable<Item> {
         nBack.next = aux2;
     }
 
-    // remove and return the item from the front
-    public Item removeFirst(){
+    // Remove and return the item from the front
+    public Item removeFirst() {
 
         if (size == 0)
             throw new NoSuchElementException();
@@ -118,12 +118,14 @@ public class Deque<Item> implements Iterable<Item> {
         size--;
         Item ret = nFront.item;
         nFront = nFront.prev;
+        if (nFront != null)
+            nFront.next = null;
 
         return ret;
     }
 
-    // remove and return the item from the back
-    public Item removeLast(){
+    // Remove and return the item from the back
+    public Item removeLast() {
 
         if (size == 0)
             throw new NoSuchElementException();
@@ -131,21 +133,24 @@ public class Deque<Item> implements Iterable<Item> {
         size--;
         Item ret = nBack.item;
         nBack = nBack.next;
+        if (nBack != null)
+            nBack.prev = null;
 
         return ret;
     }
 
-    // return an iterator over items in order from front to back
-    public Iterator<Item> iterator(){
+    // Return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
         return new DequeIterator();
     }
 
     private class DequeIterator implements Iterator<Item> {
 
+        // 'Head' of linked list
         Node current = nFront;
 
         public boolean hasNext() {
-            return (current != null) ;
+            return (current != null);
         }
 
         public Item next() {
@@ -153,6 +158,7 @@ public class Deque<Item> implements Iterable<Item> {
             if (!hasNext())
                 throw new NoSuchElementException();
 
+            // Going backwards
             Item ret = current.item;
             current = current.prev;
             return ret;
@@ -164,62 +170,56 @@ public class Deque<Item> implements Iterable<Item> {
 
     }
 
-    // unit testing (required)
-    public static void main(String[] args){
+    // Unit testing
+    public static void main(String[] args) {
 
         // javac-algs4 Deque.java && java-algs4 Deque
 
         Deque<Integer> teste = new Deque<Integer>();
 
-        // Fazer o unit test ser um while infinito,
-        // Tipo queries da maratona
-
         StdOut.println("\nComandos:\n'AF valor' - Adiciona valor na frente;\n"
-        + "'AB valor' - Adiciona valor no começo;\n" +
-        "'RF' - Printa e exclui valor do começo;\n" +
-        "'RF' - Printa e exclui valor do começo;\n" +
-        "'P' - Printa todos os elementos (Front -> Back);\n" +
-        "'0' - Finaliza unit test.\n" +
-        "OS VALORES INSERIDOS DEVEM SER INTEIROS.\n");
+                + "'AB valor' - Adiciona valor no começo;\n" + "'RF' - Printa e exclui valor do fim;\n"
+                + "'RB' - Printa e exclui valor do começo;\n" + "'P' - Printa todos os elementos (Front -> Back);\n"
+                + "'0' - Finaliza unit test.\n" + "OS VALORES INSERIDOS DEVEM SER INTEIROS.\n");
 
+        // Userr iterative test
         while (true) {
             String op = StdIn.readString();
 
-            if (op.equals("0"))
+            if (op.equals("0")) {
+                StdOut.println("Tamaho final: " + teste.size());
                 break;
+            }
 
-            if (op.equals("AF")){
+            if (op.equals("AF")) {
                 int v = StdIn.readInt();
 
                 teste.addFirst(v);
             }
 
-            else if (op.equals("AB")){
+            else if (op.equals("AB")) {
                 int v = StdIn.readInt();
 
                 teste.addLast(v);
             }
 
-            else if (op.equals("RF")){
+            else if (op.equals("RF")) {
                 Integer aux = teste.removeFirst();
 
                 StdOut.println(aux);
             }
 
-            else if (op.equals("RB")){
-                Integer aux = teste.removeFirst();
+            else if (op.equals("RB")) {
+                Integer aux = teste.removeLast();
 
                 StdOut.println(aux);
             }
 
-            else if (op.equals("P")){
+            else if (op.equals("P")) {
 
                 for (Integer p : teste)
                     StdOut.println(p);
             }
         }
-
-
     }
-
 }
--- EP04/RandomizedQueue.java
+++ EP04/RandomizedQueue.java
@@ -1,3 +1,4 @@
+
 /****************************************************************
     Nome: Felipe Castro de Noronha
     NUSP: 10737032
@@ -32,36 +33,157 @@ import java.lang.IllegalArgumentException;
 import java.lang.UnsupportedOperationException;
 
 import java.util.NoSuchElementException;
-import java.util.Arrays;
 import java.util.Iterator;
 
 import edu.princeton.cs.algs4.StdOut;
-
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    // construct an empty randomized queue
-    public RandomizedQueue()
+    private int size; // Size of RandomizedQueue
 
-    // is the randomized queue empty?
-    public boolean isEmpty()
+    // Node used in linked-list
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+
+    private Node head; // Head of ll
+
+    // Construct an empty randomized queue
+    public RandomizedQueue() {
+        head = null;
+        size = 0;
+    }
+
+    // Is the randomized queue empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     // return the number of items on the randomized queue
-    public int size()
+    public int size() {
+        return this.size;
+    }
 
-    // add the item
-    public void enqueue(Item item)
+    // Add the item
+    public void enqueue(Item item) {
+
+        size++;
+
+        Node add = new Node();
+        add.item = item;
+        add.next = head;
+        head = add;
+    }
 
     // remove and return a random item
-    public Item dequeue()
+    public Item dequeue() {
+
+        if (size == 0)
+            throw new NoSuchElementException();
+
+        size--;
+        // Generate random number [0, size[
+        int until = StdRandom.uniform(size);
+
+        Node walk = head;
+        for (int i = 0; i < until; i++)
+            walk = walk.next;
+
+        Item ret = walk.next.item;
+        walk.next = walk.next.next;
+
+        return ret;
+    }
 
     // return a random item (but do not remove it)
-    public Item sample()
+    public Item sample() {
+
+        if (size == 0)
+            throw new NoSuchElementException();
+
+        // Generate random number [0, size[
+        int until = StdRandom.uniform(size);
+
+        Node walk = head, aux;
+        for (int i = 0; i <= until; i++)
+            walk = walk.next;
+
+        return walk.item;
+    }
 
     // return an independent iterator over items in random order
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator() {
+        return new RQIterator();
+    }
+
+    private class RQIterator implements Iterator<Item> {
+
+        Node current = head;
+
+        public boolean hasNext() {
+            return (current != null);
+        }
+
+        public Item next() {
+
+            if (!hasNext())
+                throw new NoSuchElementException();
+
+            Item ret = current.item;
+            current = current.next;
+            return ret;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
 
+        // javac-algs4 RandomizedQueue.java && java-algs4 RandomizedQueue
+
+        // Fazer iterator ser aleatorio
+        // Resolver erro quando percorro a ll
+
+        RandomizedQueue<Integer> teste = new RandomizedQueue<Integer>();
+
+        StdOut.println("\nComandos:\n'ADD valor' - Adiciona valor;\n" + "'DQ' - Mostra e remove valor aleatorio;\n"
+                + "'SP' - Mostra um valor aleatorio;\n" + "'P' - Printa todos os elementos;\n"
+                + "'0' - Finaliza unit test.\n" + "OS VALORES INSERIDOS DEVEM SER INTEIROS.\n");
+
+        // Userr iterative test
+        while (true) {
+            String op = StdIn.readString();
+
+            if (op.equals("0")) {
+                StdOut.println("Tamaho final: " + teste.size());
+                break;
+            }
+
+            if (op.equals("ADD")) {
+                int v = StdIn.readInt();
+
+                teste.enqueue(v);
+            }
+
+            else if (op.equals("DQ")) {
+                StdOut.println(teste.dequeue());
+            }
+
+            else if (op.equals("SP")) {
+                StdOut.println(teste.sample());
+            }
+
+            else if (op.equals("P")) {
+
+                for (Integer p : teste)
+                    StdOut.println(p);
+            }
+        }
+    }
 }
